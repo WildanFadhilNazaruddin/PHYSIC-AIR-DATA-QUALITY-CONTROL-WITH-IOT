0ecdf1ea9a769c0f4e27d7626edd69bb3507f788
WILDAN FADHIL NAZARUDDIN <232153079@student.unsil.ac.id>
2024-12-12 17:04:38 +0000

    dart

---
+++ physic/bin/physic.dart
@@ -0,0 +1,17 @@
+import 'package:physic/physic.dart' as physic;
+
+void main(){
+  String name = "John";
+  print(name);
+  print(name);
+  print(name);
+late var valeu = getValue();
+print('Dispal Valeu');
+print(valeu);
+}
+
+String getValue(){
+  return 'Hello World';
+}
+
+
